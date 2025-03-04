7ddd88cafa6b398fbab1d83e6332ad949399271f
BenQuack <bquacken@uvm.edu>
2025-03-04 15:43:33 -0500

    colors and qol fixes

--- scripts.js
+++ scripts.js
@@ -1,2 +1,11 @@
+function moveGradient(e){
+    const x = e.clientX;
+    const y = e.clientY;
+    document.body.style.background = `radial-gradient(100px at ${x}px ${y}px, grey, white)`;
+}
 
-
+function scrollGradient(){
+    const y = window.scrollY;
+    const x = window.scrollX;
+    document.body.style.background = `radial-gradient(100px at ${x}px ${y}px, grey, white)`;
+}
